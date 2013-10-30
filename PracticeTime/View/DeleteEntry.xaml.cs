35113aa50ea3390f978c3014b0b2bfa9c56b25a5
Barrett Simms <wbsimms@gmail.com>
2013-10-30 15:35:44 -0400

    Add other pages

--- PracticeTime/View/DeleteEntry.xaml.cs
+++ PracticeTime/View/DeleteEntry.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using PracticeTime.Common;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,30 +13,44 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-using PracticeTime.Common;
-using PracticeTime.View;
 
-namespace PracticeTime
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace PracticeTime.View
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// A basic page that provides characteristics common to most applications.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class DeleteEntry : Page
     {
+
         private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
         public NavigationHelper NavigationHelper
         {
             get { return this.navigationHelper; }
         }
 
-        public MainPage()
+
+        public DeleteEntry()
         {
             this.InitializeComponent();
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += navigationHelper_LoadState;
             this.navigationHelper.SaveState += navigationHelper_SaveState;
-
         }
 
         /// <summary>
@@ -64,24 +80,27 @@ namespace PracticeTime
         {
         }
 
-        private void EntryButtonClick(object sender, RoutedEventArgs e)
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            Button b = sender as Button;
-            if (b != null)
-            {
-                if (b.Name == "AddEntry") NavigateTo(typeof(AddEntry));
-                if (b.Name == "UpdateEntry") NavigateTo(typeof(UpdateEntry));
-                if (b.Name == "DeleteEntry") NavigateTo(typeof(DeleteEntry));
-            }
+            navigationHelper.OnNavigatedTo(e);
         }
 
-        private void NavigateTo(Type t)
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-            if (rootFrame != null)
-            {
-                rootFrame.Navigate(t);
-            }
+            navigationHelper.OnNavigatedFrom(e);
         }
+
+        #endregion
     }
 }
