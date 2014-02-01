6ca8c0e4ba6a744582e3cd3696394bd4af6b521a
Barrett Simms <wbsimms@gmail.com>
2014-02-01 13:43:22 -0500

    Add credentails

--- src/Web/PracticeTime.Web/App_Start/Startup.Auth.cs
+++ src/Web/PracticeTime.Web/App_Start/Startup.Auth.cs
@@ -19,20 +19,20 @@ namespace PracticeTime.Web
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            //Uncomment the following lines to enable logging in with third party login providers
+            app.UseMicrosoftAccountAuthentication(
+                clientId: "0000000048105430 ",
+                clientSecret: "/8rj8oj4eFmGDyKew/65oDg67L3xWRbl ");
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            app.UseTwitterAuthentication(
+               consumerKey: "NcBhP7nE3SjMQHcRhjBQ",
+               consumerSecret: "YYZqrESCgp35fPwAGqiNN2XybJuR9mpsA7PwPlRgZEg");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "227198527467491",
+               appSecret: "3265c528e316ae4f405ecee44e3bae6d");
 
-            //app.UseGoogleAuthentication();
+            app.UseGoogleAuthentication();
         }
     }
 }
