71edc3d9ebf09a061f4f8acc94804306ef269d25
Barrett Simms <wbsimms@gmail.com>
2014-02-07 11:48:23 -0500

    Coverage

---
+++ src/Web/PracticeTime.Web.DataAccess.Test/Repositories/BadgeRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeTime.Web.DataAccess.Models;
+using PracticeTime.Web.DataAccess.Repositories;
+
+namespace PracticeTime.Web.DataAccess.Test.Repositories
+{
+    [TestClass]
+    public class BadgeRepositoryTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            BadgeRepository repo = new BadgeRepository();
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+            BadgeRepository repo = new BadgeRepository();
+            List<C_Badge> badges = repo.GetAll();
+            Assert.IsTrue(badges.Count > 0);
+        }
+
+        [TestMethod]
+        public void GetByIdTest()
+        {
+            BadgeRepository repo = new BadgeRepository();
+            C_Badge badge = repo.GetById(1);
+            Assert.IsNotNull(badge);
+            Assert.AreEqual("First Session", badge.Name);
+        }
+
+    }
+}
