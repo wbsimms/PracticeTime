f2f059643a01bedd215b21f6354925b4fb6f3e2e
Barrett Simms <wbsimms@gmail.com>
2014-02-08 15:08:10 -0500

    Coverage

---
+++ src/Web/PracticeTime.Web.Test/Helpers/UserHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeTime.Web.Helpers;
+
+namespace PracticeTime.Web.Test.Helpers
+{
+    [TestClass]
+    public class UserHelperTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            UserHelper helper = new UserHelper();
+            Assert.IsNotNull(helper);
+        }
+
+        [TestMethod]
+        public void GetUserIdTest()
+        {
+            UserHelper helper = new UserHelper();
+            string userId = helper.GetUserId("wbsimms");
+            Assert.IsNotNull(userId);
+            Assert.IsTrue(!string.IsNullOrEmpty(userId));
+
+        }
+    }
+}
