be5c9c750513d487ca868f55102361eaca4ccfdd
Barrett Simms <wbsimms@gmail.com>
2014-02-08 14:52:55 -0500

    Coverage

---
+++ src/Web/PracticeTime.Web.DataAccess.Test/PracticeTimeDataAccessResolverTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PracticeTime.Web.DataAccess.Test
+{
+    [TestClass]
+    public class PracticeTimeDataAccessResolverTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            PracticeTimeDataAccessResolver instance = PracticeTimeDataAccessResolver.Instance;
+            Assert.IsNotNull(instance);
+        }
+
+        [TestMethod]
+        public void ContainerTest()
+        {
+            PracticeTimeDataAccessResolver instance = PracticeTimeDataAccessResolver.Instance;
+            Assert.IsNotNull(instance.Container);
+        }
+
+    }
+}
