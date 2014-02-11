d347db625927f011f9cb12b66e01c2b51beb4816
Barrett Simms <wbsimms@gmail.com>
2014-02-11 12:00:39 -0500

    Add meta tag and unit tests

---
+++ src/Web/PracticeTime.Web.DataAccess.Test/Models/ApplicationUserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeTime.Web.DataAccess.Models;
+
+namespace PracticeTime.Web.DataAccess.Test.Models
+{
+    [TestClass]
+    public class ApplicationUserTest
+    {
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            ApplicationUser user = new ApplicationUser();
+            user.C_AccountTypeId = 4;
+            user.C_AccountType = new C_AccountType();
+            Assert.IsNotNull(user);
+            Assert.IsNotNull(user.C_AccountType);
+            Assert.AreEqual(4,user.C_AccountTypeId);
+        }
+    }
+}
---
+++ src/Web/PracticeTime.Web.DataAccess.Test/Models/BadgeAwardTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeTime.Web.DataAccess.Models;
+
+namespace PracticeTime.Web.DataAccess.Test.Models
+{
+    [TestClass]
+    public class BadgeAwardTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            BadgeAward badgeAward = new BadgeAward(){User = new ApplicationUser()};
+            Assert.IsNotNull(badgeAward);
+            Assert.IsNotNull(badgeAward.User);
+        }
+    }
+}
