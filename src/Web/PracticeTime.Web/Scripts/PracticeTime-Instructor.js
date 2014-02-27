29adf17f35d6f222d3ba809e85d5d4d7dd32ed1a
Barrett Simms <wbsimms@gmail.com>
2014-02-27 08:31:40 -0500

    Fix instructor display

--- src/Web/PracticeTime.Web/Scripts/PracticeTime-Instructor.js
+++ src/Web/PracticeTime.Web/Scripts/PracticeTime-Instructor.js
@@ -3,7 +3,6 @@
 $(document).ready(function() {
     getSessionsForStudent();
 
-
     $("#SelectedStudent").change(function() {
         getSessionsForStudent();
     });
@@ -21,7 +20,6 @@ $(document).ready(function() {
             $("#delete-message").removeClass("hidden");
             $("#SelectedRegisteredStudent option:selected").remove();
         });
-
     });
 
     function getSessionsForStudent() {
@@ -46,14 +44,10 @@ $(document).ready(function() {
             sessionData.append('<tr><td>No Data</td><td/><td/><td/></tr>');
         } else {
             for (var row in jdata) {
-                var date = new Date(Date.parse(jdata[0]['SessionDateTimeUtc']));
+                var date = new Date(Date.parse(jdata[row]['SessionDateTimeUtc']));
                 var givenDate = $.datepicker.formatDate('mm/dd/yy', date);
-                sessionData.append('<tr><td>' + jdata[0]['C_Instrument']['Name'] + '</td><td>' + jdata[0]['Title'] + '</td><td>' + givenDate + '</td><td>' + jdata[0]['Time'] + '</td></tr>');
+                sessionData.append('<tr><td>' + jdata[row]['C_Instrument']['Name'] + '</td><td>' + jdata[row]['Title'] + '</td><td>' + givenDate + '</td><td>' + jdata[row]['Time'] + '</td></tr>');
             }
-
         }
-
     }
-
-
 });
