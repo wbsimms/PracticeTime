389920442edd059ace9cdcd59b049f1fe0dff17c
Barrett Simms <wbsimms@gmail.com>
2014-03-06 09:44:47 -0500

    US5

---
+++ src/Web/PracticeTime.Web.Test/Models/NameValueTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeTime.Web.Models;
+
+namespace PracticeTime.Web.Test.Models
+{
+    [TestClass]
+    public class NameValueTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            NameValue nameValue = new NameValue(){Name="foo",Value = "bar"};
+            Assert.IsNotNull(nameValue.Name);
+            Assert.IsNotNull(nameValue.Value);
+        }
+    }
+}
